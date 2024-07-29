288d5143c05b114d694840d8bbe0696213c51bb4
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-29 01:18:52 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -2,10 +2,11 @@
 
 {
     'name': 'Custom Product Label',
-    'version': '1.1',
+    'version': '1.0',
     'depends': ['base', 'product'],
     'data': [
         'report/product_label_template.xml',
         'views/product_label_view.xml',
     ],
+    'license': 'LGPL-3',
 }
